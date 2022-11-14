0fcd9a996d8e9d52cc89309e2a3bc59908ac1ac3
Shade Emsigima <36532694+ShadeEmsigima@users.noreply.github.com>
2022-11-14 12:52:00 +0100

    element in view function works

--- functions.js
+++ functions.js
@@ -4,34 +4,33 @@ function checkPos(){
     const cIntroduction = document.getElementById('introduction');
     const cCards = document.getElementById('cards');
     
-    //track where the user is currently viewing
-    var userPos = visualViewport.pageTop
+    //track the middle of user's current view
+    var userPos = visualViewport.pageTop + (visualViewport.height/2)
 
     //track which is the currently viewed element
     var elem
 
+    //calc if pos is within element's bottom and top
     if(userPos <= cGreet.offsetHeight && userPos >= cGreet.offsetTop){
         elem = cGreet  
         console.log(elem.id)
     }
-    else if (userPos <= cIntroduction.offsetHeight && userPos >= cIntroduction.offsetTop){
+    else if (userPos <= cIntroduction.offsetTop + cIntroduction.offsetHeight && userPos >= cIntroduction.offsetTop){
         elem = cIntroduction
         console.log(elem.id)
     }
-    else if (userPos <= cCards.offsetHeight && userPos >= cCards.offsetTop){
+    else {
         elem = cCards
         console.log(elem.id)
     }
-    //elem.document.style.setProperty.opacity(1)
+    
+    elem.style.opacity = 1;
 
-
-    //console.log(visualViewport.pageTop) //determine element pos
+    console.log("--------------------" + visualViewport.pageTop + " - " + visualViewport.height) //determine element pos
    // console.log("top: " + elem.offsetTop + " offset height: " + elem.offsetHeight)  //determen user's position
 }
 
-function updateDot{
-    SVGAElement.re
-}
+
 
 function hi(){
     //console.log("HERE! OVER HERE!")
