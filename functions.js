dc74851cefc82dd8542a0b63d392c3c87d622943
Shade Emsigima <36532694+ShadeEmsigima@users.noreply.github.com>
2022-11-14 12:19:50 +0100

    wip fade in on scroll over

--- functions.js
+++ functions.js
@@ -1,17 +1,39 @@
-function easeIn(){
-    console.log("doing a thing")
-    const elem = document.getElementById('kip');
+function checkPos(){
+    //save all sections in const
+    const cGreet = document.getElementById('greet');
+    const cIntroduction = document.getElementById('introduction');
+    const cCards = document.getElementById('cards');
     
-    if(visualViewport.pageTop<=elem.offsetHeight && visualViewport.pageTop>=elem.offsetTop){
-        console.log("viewing " + elem.id)
-        if(elem.children.opacity == 0) console.log("opacity = 0")
-        else{console.log("opacity != 0")}
+    //track where the user is currently viewing
+    var userPos = visualViewport.pageTop
+
+    //track which is the currently viewed element
+    var elem
+
+    if(userPos <= cGreet.offsetHeight && userPos >= cGreet.offsetTop){
+        elem = cGreet  
+        console.log(elem.id)
     }
-        
-    else
-        console.log("not in view")
+    else if (userPos <= cIntroduction.offsetHeight && userPos >= cIntroduction.offsetTop){
+        elem = cIntroduction
+        console.log(elem.id)
+    }
+    else if (userPos <= cCards.offsetHeight && userPos >= cCards.offsetTop){
+        elem = cCards
+        console.log(elem.id)
+    }
+    //elem.document.style.setProperty.opacity(1)
 
 
     //console.log(visualViewport.pageTop) //determine element pos
-    //console.log("top: " + elem.offsetTop + " offset height: " + elem.offsetHeight)  //determen user's position
+   // console.log("top: " + elem.offsetTop + " offset height: " + elem.offsetHeight)  //determen user's position
+}
+
+function updateDot{
+    SVGAElement.re
+}
+
+function hi(){
+    //console.log("HERE! OVER HERE!")
+    //html: onwheel="hi()"
 }
